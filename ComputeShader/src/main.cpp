97e23abf22134ca2cfcf904ca3edbdf77f1d8957
Robert <giuffredarobert@gmail.com>
2021-08-12 22:58:24 -0400

    Initial commit

---
+++ ComputeShader/src/main.cpp
@@ -0,0 +1,38 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+int main(void)
+{
+	GLFWwindow* window;
+
+	/* Initialize the Library*/
+	if (!glfwInit())
+		return -1;
+
+	/* Create Windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(1080, 720, "Hello World!", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make windows context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events*/
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	return 0;
+}
